9173488ecbef0fe39cd806b48454d926e0de1a89
faisalalkholide1-cp <faisalalkholide1@gmail.com>
2026-06-07 01:04:39 +0300

    Correcting the error of retrieving available representative data

--- modules/couriers/src/Couriers.Application/Mapping/CourierAutoMapperProfile.cs
+++ modules/couriers/src/Couriers.Application/Mapping/CourierAutoMapperProfile.cs
@@ -15,3 +15,11 @@ public partial class CourierAutoMapperProfile : MapperBase<CourierProfile, Couri
     public override partial void Map(CourierProfile source, CourierProfileDto destination);
 
 }
+
+[Mapper]
+public partial class CourierLookupMapperProfile : MapperBase<CourierProfile, CourierLookupDto>
+{
+    public override partial CourierLookupDto Map(CourierProfile parcel);
+
+    public override partial void Map(CourierProfile source, CourierLookupDto destination);
+}
